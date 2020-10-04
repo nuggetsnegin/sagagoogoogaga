89c4bce86f7aedc68774d38944c21bbb9cc0d558
Negin <nuggetnegin@gmail.com>
2020-10-04 14:09:19 -0400

    canvas and cat body set up

---
+++ components/Canvas.js
@@ -0,0 +1,22 @@
+export default function Canvas(){
+
+
+    return(
+        <div className="container">
+            <h2><button>(ﾉΦωΦ)ﾉ</button></h2>
+            <style jsx>{`
+                button{
+                    font-size: 10rem;
+                    border: none;
+                    background: none;
+                }
+                button:hover,
+                button:focus{
+                    cursor: grabbing;
+                }
+            `}</style>
+        </div>
+
+        
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
-function *loopSaying(){
-  yield 'hello'
-  yield 'goodbye'
-}
-
-const loopGenerator = loopSaying()
-
-console.log(loopGenerator.next().value);
+import Canvas from '../components/Canvas'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Cat clicker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>??????????????????????</h1>
-        <p>{loopGenerator.next().value}</p>
+        <h1>Cat Clicker</h1>
+        <Canvas/>
       </main>
     </div>
   )
