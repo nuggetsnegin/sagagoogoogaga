5233a1485118add538ef5b4275fae68a082562ad
Negin Sauermann <neginsauermann@Negins-MacBook-Pro.local>
2020-10-07 13:43:32 -0400

    added cleanliness saga

--- sagas/saga.js
+++ sagas/saga.js
@@ -1,4 +1,4 @@
-import { call, put, all, delay } from 'redux-saga/effects'
+import { call, put, all, delay, takeEvery } from 'redux-saga/effects'
 
 export function* incrementHunger(){
     yield delay(1000)
@@ -10,10 +10,16 @@ export function* decreaseHappiness(){
     yield put({type: 'DECREASE_HAPPINESS'})
 }
 
+export function* decreaseCleanliness(){
+  yield delay(9000)
+  yield put({type: 'DECREASE_CLEANLINESS'})
+}
+
 //single ppont of entry for all sagas
 export default function* rootSaga() {
     yield all([
       call(incrementHunger),
       call(decreaseHappiness),
+      call(decreaseCleanliness)
     ])
   }
