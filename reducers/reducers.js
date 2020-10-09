38c20fc7228489a9e4894c72c9fe42c8295d654c
Negin Sauermann <negin.s@earnwithdrop.com>
2020-10-09 16:33:16 -0400

    refactored store to use useRef and get current ref to obj

--- reducers/reducers.js
+++ reducers/reducers.js
@@ -1,26 +1,14 @@
 import u from 'updeep'
 
 const initialState = {
-    peepeepoopoo: {
-        pee: {
-            poo: true
-        }
-    },
     happiness: 0,
     hunger: 0,
     clean: 0,
 }
 
-//SIMON encourages to use updeep 
-//obj you want to update the first arg is a 'deep partial' (obj nested within obj, dont wanna have to copy new versions of that obj)
-//updeep abstracts that 
 export default function tama(state = initialState, action){
     switch(action.type){
         case 'PLAY_TAMA':
-            //copied state and then peepeepoopoo is just a regular partial of the piece of state (slice of the redux store but its an entire piece of the redux store)
-            //return{...state, peepeepoopoo: {...state.peepeepoopoo, pee: {...state.peepeepoopoo.pee, poo: false}}}
-            //deep partial, partial slice of your initial state at an arbritary level of nesting
-            //return u({peepeepoopoo: {pee: {poo: false}}}, state)
             return u({happiness: state.happiness +1}, state)
         case 'FEED_TAMA':
             return u({hunger: state.hunger -5}, state)
